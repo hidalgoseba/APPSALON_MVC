f7966f5994b6d90b7e59b32e5291713d06eefafc
Sebastian Hidalgo <hidalgoseba@hotmail.com>
2022-02-16 13:20:05 -0300

    Reparar database para funcionar app

--- includes/database.php
+++ includes/database.php
@@ -2,8 +2,6 @@
 
 $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
 
-
-
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
     echo "errno de depuración: " . mysqli_connect_errno();
