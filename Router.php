30cd9978327086c8b1c8343e3e6f278aa1ab2262
Sebastian Hidalgo <hidalgoseba@hotmail.com>
2022-02-21 18:22:55 -0300

    prueba en router no impriome servicios

--- Router.php
+++ Router.php
@@ -28,26 +28,26 @@ class Router
 
         //$auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
-        } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
-        }
-        //$currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'];
-        //$method = $_SERVER['REQUEST_METHOD'];
-
-        // //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        // $splitURL = explode('?', $currentUrl);
-        // // debuguear($splitURL);
+        // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        // $method = $_SERVER['REQUEST_METHOD'];
 
         // if ($method === 'GET') {
-        //     $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+        //     $fn = $this->getRoutes[$currentUrl] ?? null;
         // } else {
-        //     $fn = $this->postRoutes[$splitURL[0]] ?? null;
+        //     $fn = $this->postRoutes[$currentUrl] ?? null;
         // }
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'];
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+        $splitURL = explode('?', $currentUrl);
+        // debuguear($splitURL);
+
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+        } else {
+            $fn = $this->postRoutes[$splitURL[0]] ?? null;
+        }
 
 
         if ( $fn ) {
