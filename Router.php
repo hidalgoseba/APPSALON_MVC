06220d634107286d755e939ec4c7376f98275c7b
Sebastian Hidalgo <hidalgoseba@hotmail.com>
2022-02-24 01:46:29 -0300

    prueba17

--- Router.php
+++ Router.php
@@ -19,25 +19,13 @@ class Router
 
     public function comprobarRutas()
     {
-        // $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'];
-        // $method = $_SERVER['REQUEST_METHOD'];
-
-        // if ($method === 'GET') {
-        //     $fn = $this->getRoutes[$currentUrl] ?? null;
-        // } else {
-        //     $fn = $this->postRoutes[$currentUrl] ?? null;
-        // }
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'];
+        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
-        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
-        $splitURL = explode('?', $currentUrl);
-        //debuguear($splitURL);
-
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+            $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
-            $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
         if ($fn) {
