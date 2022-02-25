c94559166981ea2cad9ab127b379bff68939d99c
Sebastian Hidalgo <hidalgoseba@hotmail.com>
2022-02-24 21:45:28 -0300

    correccion del envio del mail para confirmar y recuperar cuenta

--- classes/Email.php
+++ classes/Email.php
@@ -35,7 +35,7 @@ class Email {
         $mail->CharSet = 'UTF-8';
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en App Salon, solo debes confirmar la cuenta presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='https://enigmatic-river-73959.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>Si no solicitaste esta cuenta, ignora el mensaje</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
@@ -64,7 +64,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Solicistaste reestablecer tu password, sigue el siguiente enlace y hazlo.</p>";
-        $contenido .= $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
+        $contenido .= $contenido .= "<p>Presiona aquí: <a href='https://enigmatic-river-73959.herokuapp.com/recuperar?token=" . $this->token . "'>Reestablecer Password</a></p>";
         $contenido .= "<p>Si no solicitaste esta cuenta, ignora el mensaje</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
